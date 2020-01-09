57e1fde107e12c5d4a9a5acf93d37ffd66284208
Phoenix--Project <thephoenixproject208@gmail.com>
2020-01-09 19:04:29 +0000

    Example Plugin

---
+++ ExamplePlugins/ExamplePlugin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+using PheggMod.API.Plugin;
+using PheggMod.API.Events;
+
+namespace ExamplePlugin
+{
+    [Plugin.PluginDetails(
+    author = "ThePheggHimself",
+    name = "TestPlugin",
+    description = "Test plugin for Pheggmod",
+    version = "1.0_BETA"
+    )]
+
+    public class ExamplePlugin : Plugin
+    {
+        public override void initializePlugin()
+        {
+            Info("PLUGIN LOADED!!!!");
+
+            this.AddEventHandlers(new Playerhurt(this));
+        }
+    }
+}
