7fded3db3fd096f3bf16b6e2277b6fec9d743ef6
Phoenix--Project <thephoenixproject208@gmail.com>
2020-04-16 00:39:44 +0100

    Fixed, updated and re-enabled the auto-restart system.

--- PheggMod/PheggModBase.cs
+++ PheggMod/PheggModBase.cs
@@ -21,19 +21,35 @@ namespace PheggMod
         private static string _serverName = string.Empty;
         private bool _quitting = false;
         private static bool _debug;
+        private string _restartTime;
+        private int[] _restartTimeClean;
 
         private extern void orig_FixedUpdate();
 
         private new void FixedUpdate()
         {
-            //if(!_quitting && DateTime.Now.Hour == 5 && DateTime.Now.Minute == 30 && Time.realtimeSinceStartup > 60)
-            //{
-            //    AddLog("[PHEGGMOD] Restarting server!");
+            if (!_quitting && !string.IsNullOrEmpty(_restartTime))
+            {
+                if(_restartTimeClean == null || _restartTimeClean.Length == 0)
+                {
+                    string [] array = _restartTime.Split(':');
+                    array[1].Replace(":", string.Empty);
 
-            //    _quitting = true;
-            //    //Application.Quit();
-            //    GameCore.Console.singleton.TypeCommand("QUIT");
-            //}
+                    _restartTimeClean = array.Select(x => Int32.Parse(x)).ToArray();
+                }
+
+                DateTime dTime = DateTime.Now;
+
+                ///Checking for the startup time is simply a safety feature to ensure that the server isn't stuck in a boot loop for the while minute duration specified for the restart
+                ///It's set for 60 seconds to ensure that the whole minute passes, meaning that regardless of server specs, it is impossible to start looping :D
+                if (dTime.Hour == _restartTimeClean[0] && dTime.Minute == _restartTimeClean[1] && Time.realtimeSinceStartup > 60)
+                {
+                    AddLog($"[PHEGGMOD] Server time has reached {dTime.ToShortTimeString()}, so the server will now restart!");
+
+                    _quitting = true;
+                    GameCore.Console.singleton.TypeCommand("QUIT");
+                }
+            }
 
             orig_FixedUpdate();
         }
@@ -48,13 +64,14 @@ namespace PheggMod
         public void Start()
         {
             orig_Start();
+
             CustomNetworkManager.Modded = true;
-
-            new SmartGuard();
-
             AddLog("[PHEGGMOD] THIS SERVER IS RUNNING PHEGGMOD");
 
-            _debug = ConfigFile.ServerConfig.GetBool("pheggmod_debug", true);
+            //Commented out until SmartGuard is fixed
+            //new SmartGuard();
+
+            _debug = ConfigFile.ServerConfig.GetBool("pheggmod_debug", false);
 
             if (_debug)
             {
@@ -65,6 +82,11 @@ namespace PheggMod
                     + $"\nUserIdWhitelist.txt: {ConfigSharing.Paths[2] + "UserIDWhitelist.txt"}"
                     + $"\nUserIdReservedSlots.txt: {ConfigSharing.Paths[3] + "UserIDReservedSlots.txt"}");
             }
+
+            ///This is the time that the server will check for with the auto-restarting system.
+            ///Uses 24 hour formatting (16:00 is 4PM), and uses the time relative to the server.
+            ///Keeping as null will disable;
+            _restartTime = ConfigFile.ServerConfig.GetString("auto_restart_time", null);
 
             new Commands.CustomInternalCommands();
             PluginManager.PluginPreLoad();
