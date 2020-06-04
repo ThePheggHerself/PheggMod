5fbf8f579ccc781fe4c4f9ffe2055b48b0628f27
Phoenix--Project <thephoenixproject208@gmail.com>
2020-06-04 22:41:56 +0100

    Changed the 096 target message to be a "hint" rather than a broadcast (so the text appears under the central recticle, rather than the top of the screen)

--- PheggMod/EventTriggers/Scp096.cs
+++ PheggMod/EventTriggers/Scp096.cs
@@ -1,6 +1,7 @@
 ﻿#pragma warning disable CS0626 // orig_ method is marked external and has no attributes on it.
 using CustomPlayerEffects;
 using GameCore;
+using Hints;
 using MonoMod;
 using PheggMod.API.Events;
 using PlayableScps;
@@ -39,7 +40,8 @@ namespace PheggMod.EventTriggers
             {
                 if (HasTarget(hub) && !warnedTargets.Contains(id))
                 {
-                    new PheggPlayer(target).PersonalBroadcast(5, "You are now a target for 096!");
+                    target.GetComponent<HintDisplay>().Show(new TextHint("You are now a target for 096!", new HintParameter[] { new StringHintParameter("") }, HintEffectPresets.FadeInAndOut(1, 2.5f, 1.5f), 8));
+                    //new PheggPlayer(target).PersonalBroadcast(5, "You are now a target for 096!");
                     warnedTargets.Add(id);
                 }
             }
